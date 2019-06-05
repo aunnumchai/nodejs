cee0d913db3599f33009884002d2c7b48fef56f4
aunnumchai <numchai.aun@gmail.com>
2019-06-05 10:35:51 +0700

    insert data

--- connect.js
+++ connect.js
@@ -1,21 +1,29 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Sundae1234#',
-  database : 'nodejs'
-});
+// var mysql      = require('mysql');
+// var con = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'Sundae1234#',
+//   database : 'nodejs'
+// });
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT * FROM tbl_node1', function (error, results, fields) {
-  if (error) throw error;
-//   console.log('The solution is: ', results);
-  results.forEach(
-  (element, index, array) => 
-    //   console.log(element, index, array)
-      console.log(element.userName)
-  )
-});
+// connection.query('SELECT * FROM tbl_node1', function (error, results, fields) {
+//   if (error) throw error;
+//   results.forEach(
+//   (element, index, array) => 
+//       console.log(element.userName)
+//   )
+// });
 
-connection.end();
+// connection.end();
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO tbl_node1 (userName) VALUES ('Company Inc')";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
--- server.js
+++ server.js
@@ -9,23 +9,49 @@ app.use(bodyParser.json())
 
 store.init()
 
-app.get('/', (req, res) => {
-    res.send('Hello World2!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World2!')
+// })
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Sundae1234#',
+  database : 'nodejs'
+});
 
 // app.get('/item/', (req, res) => {
 //   res.send({ items: store.getAllItems() })
 // })
 
-// app.get('/item/:index', (req, res) => {
-//   const item = store.getItem(Number(req.params.index))
- 
-//   if (item === undefined) {
-//     res.status(404).end()
-//     return
-//   }
-//   res.send({ item })
-// })
+app.get('/item/:index', (req, res) => {
+  
+  connection.connect();
+  // const item = store.getItem(Number(req.params.index))
+  connection.query('SELECT * FROM tbl_node1', function (error, results, fields) {
+    if (error) throw error;
+    var item = "";
+    // results.forEach(
+    // (element, index, array) => 
+    //     // console.log(element.userName)
+        // item = element.userName
+    // )
+    item = results;
+    // console.log(results);
+    if (item === undefined) {
+      res.status(404).end()
+      return
+    }
+    res.send({ item })
+    
+  });
+  // console.log(1);
+  // connection.end();
+  // const item = store.getItem(Number(req.params.index))
+  // var item = "1";
+  
+})
 
 // app.post('/item/', (req, res) => {
 //   if (typeof req.body.item !== 'string') {
@@ -54,16 +80,16 @@ app.get('/', (req, res) => {
 //   res.send({ oldItem })
 // })
 
-app.delete('/item/:index', (req, res) => {
-  if (store.getItem(Number(req.params.index)) === undefined) {
-    res.status(404).end()
-    return
-  }
+// app.delete('/item/:index', (req, res) => {
+//   if (store.getItem(Number(req.params.index)) === undefined) {
+//     res.status(404).end()
+//     return
+//   }
  
-  const removeItem = store.removeItem(Number(req.params.index))
+//   const removeItem = store.removeItem(Number(req.params.index))
  
-  res.send({ removeItem })
-})
+//   res.send({ removeItem })
+// })
  
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
